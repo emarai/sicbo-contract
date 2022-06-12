1aacb45a4d65d216f77a233230f070d10254a838
Irfianto <irfi@paras.id>
2022-06-12 22:01:02 +0700

    feat: play dices

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,13 @@ pub struct SicBoResult {
 }
 
 #[derive(Serialize, Deserialize)]
+pub struct DiceResult {
+    account_id: AccountId,
+    dice_value: u8,
+    total_winning: Balance,
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct RouletteResult {
     account_id: AccountId,
     roulette_value: u8,
@@ -147,13 +154,25 @@ impl Contract {
         let account_id = env::predecessor_account_id();
         let mut account = self.users.get(&account_id.to_string()).expect("Account Id does not exist");
 
-        assert!(bet <= account.deposit, "Account deposit does not sufficient for the bet");
+        assert!(bet.0 <= account.deposit, "Account deposit does not sufficient for the bet");
 
-        let mut total: u128 = 0;
         let dice_value: u8 = dice_100_roll();
-        if dice_value < roll_under {
-            let times: f64 = 98.50/(roll_under-1);
+        let total_winning: u128 = if dice_value < roll_under {
+            let times: f64 = 98.50/(roll_under as f64 -1.0);
+            ((times * 1000.0) as u128 * bet.0) / 1000u128
+        } else {
+            0u128
+        };
+        account.deposit = account.deposit - bet.0 + total_winning;
+        self.house = self.house + bet.0 - total_winning;
+        self.users.insert(&account_id, &account);
+
+        DiceResult {
+            account_id,
+            dice_value,
+            total_winning
         }
+
     }
 }
 
